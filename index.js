22ff937116a00bed7a74fea0622a4dabf2c95a42
kevinsmithseven <kevinsmithseven@gmail.com>
2023-10-11 19:50:59 -0500

    added required packages, created questions inquirer variable in index.js

--- index.js
+++ index.js
@@ -1,13 +1,56 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'Please enter the title of your project:',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'Please enter a description for you project:',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'Please enter the installation instructions for your project:',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'Please enter the usage information for this project:',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'Please enter any contribution information or guidelines you wish to include:',
+            name: 'contribution'
+        },
+        {
+            type: 'input',
+            message: 'Please enter any testing instruction information you wish to include',
+            name: 'testing',
+        }
+    ])
+        .then((response) => {
+        console.log(response);
+    })
+
+    switch (questions) {
+        
+    }
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
